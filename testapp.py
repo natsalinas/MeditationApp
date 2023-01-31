0d7833ab1732b208edbcbaa1fe599e26186109c9
Nat <nathaliasalinas97@gmail.com>
2023-01-31 07:33:38 -0500

    updated background to sky blue

--- testapp.py
+++ testapp.py
@@ -1,7 +1,7 @@
 #create and launch a kivy application
 from kivy.app import App 
 from kivy.uix.label import Label #allow us to type title to screen
-
+from kivy.core.window import Window
 #use Object Oriented Programming  
 #BasicApp inherits all properties of App class 
 class BasicApp(App):
@@ -9,6 +9,7 @@ class BasicApp(App):
     # root widget = parent widget
     # we only have the label as a widget 
     def build(self): 
+        Window.clearcolor = (0,191,255) #rgb value for color of app
         label = Label(text="Hello World \nThis is my first app.")
         return label 
 
